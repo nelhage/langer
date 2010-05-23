44f907535507f50b069ad2605f2a6b3f36f475a0
Nelson Elhage <nelhage@ksplice.com>
2010-05-23 18:19:37 -0400

    Overwrite gmon-related functions with initial RETs.
    These get generated by -pg, but they are called by other shared
    libraries before our preload can load, and so die with SIGTRAP. We
    don't want them anyways -- we're just -pg for the mcount calls -- so
    neutralize them.

--- replace-ret.c
+++ replace-ret.c
@@ -26,15 +26,16 @@ void die(const char *err, ...) {
     exit(1);
 }
 
-struct ret_entry {
-    struct ret_entry *next;
+struct offset_list {
+    struct offset_list *next;
     file_ptr off;
 };
 
-struct ret_entry *rets = NULL;
+struct offset_list *rets = NULL;
+struct offset_list *nuke_functions = NULL;
 
 void add_ret(file_ptr off) {
-    struct ret_entry *r = malloc(sizeof *r);
+    struct offset_list *r = malloc(sizeof *r);
     r->next = rets;
     r->off = off;
     rets = r;
@@ -84,7 +85,7 @@ void do_mark_rets(bfd *abfd, asection *sect, void *data) {
 }
 
 void do_replacement(const char *path) {
-    struct ret_entry *r = rets;
+    struct offset_list *r = rets;
     FILE *f = fopen(path, "r+");
     unsigned char c;
     assert(f);
@@ -98,7 +99,49 @@ void do_replacement(const char *path) {
         fwrite(&c, 1, 1, f);
         r = r->next;
     }
+
+    r = nuke_functions;
+
+    while (r) {
+        assert(!fseek(f, r->off, SEEK_SET));
+        c = OP_RET;
+        fwrite(&c, 1, 1, f);
+        r = r->next;
+    }
+
     fclose(f);
+}
+
+void find_gmon_start(bfd *b) {
+    long storage_needed;
+    asymbol **symbol_table;
+    long number_of_symbols;
+    long i;
+
+    storage_needed = bfd_get_symtab_upper_bound (b);
+
+    assert(storage_needed >= 0);
+
+    if (storage_needed == 0)
+        return;
+
+    symbol_table = malloc(storage_needed);
+
+    number_of_symbols = bfd_canonicalize_symtab (b, symbol_table);
+
+    assert(number_of_symbols >= 0);
+
+    for (i = 0; i < number_of_symbols; i++) {
+        asymbol *sym = symbol_table[i];
+        if (sym->flags & BSF_FUNCTION &&
+            (strcmp(sym->name, "call_gmon_start") == 0 ||
+             strcmp(sym->name, "__gmon_start__") == 0)) {
+            struct offset_list *n = malloc(sizeof *n);
+            n->off = sym->section->filepos + sym->value;
+            n->next = nuke_functions;
+            nuke_functions = n;
+        }
+    }
 }
 
 int do_replace_nops(const char *path) {
@@ -106,6 +149,7 @@ int do_replace_nops(const char *path) {
     bfd_byte *contents, *patch;
     asection *rodata, *text;
     bfd_vma err_string;
+    asymbol *sym;
 
     bfd_init();
 
@@ -114,7 +158,10 @@ int do_replace_nops(const char *path) {
     assert(bfd_check_format(b, bfd_object));
 
     bfd_map_over_sections(b, do_mark_rets, NULL);
+    find_gmon_start(b);
+
     bfd_close(b);
+
     do_replacement(path);
 }
 
